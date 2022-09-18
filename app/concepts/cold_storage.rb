552684442cd9d5d3b8f8610c5865a1a784379022
Sean DMR <sean@flyinggrizzly.net>
2022-09-18 23:11:02 +0100

    Add basic ColdStorage concept

---
+++ app/concepts/cold_storage.rb
@@ -0,0 +1,27 @@
+module ColdStorage
+  extend(self)
+
+  def freeze
+    data = Kit.all.map do |kit|
+      {
+        name: kit.name,
+        identifier: kit.identifier,
+        handle: kit.handle,
+        notes: kit.notes,
+        kit_scale: kit.kit_scale.name,
+        designers: kit.designers.pluck(:name),
+        producers: kit.producers.pluck(:name),
+        materials: kit.materials.pluck(:name),
+        source_materials: kit.source_materials.pluck(:slug),
+        kit_links: kit.kit_links.pluck(:identifier),
+        kit_instances: kit.kit_instances.pluck(:status, :notes).map {|status, notes| { status:, notes: }},
+      }
+    end
+
+    Fs.write('kits', data)
+  end
+
+  def thaw
+    data = Fs.read_latest('kits')
+  end
+end
---
+++ app/concepts/cold_storage/fs.rb
@@ -0,0 +1,46 @@
+module ColdStorage
+  module Fs
+    extend(self)
+
+    STORAGE_LOCATION = "cold_storage"
+
+    def write(name, data)
+      path = todays_folder.join("#{name}__#{DateTime.current.iso8601}.json")
+
+      File.write(path, JSON.pretty_generate(data))
+    end
+
+    def read_latest(name)
+      JSON.load_file(latest_file(name))
+    end
+
+    private
+
+    def todays_folder
+      Dir.mkdir(serialized_data_repository_path) unless Dir.exist?(serialized_data_repository_path)
+
+      date_dir = serialized_data_repository_path.join(Date.current.iso8601)
+
+      Dir.mkdir(date_dir) unless Dir.exist?(date_dir)
+
+      date_dir
+    end
+
+    def latest_file(prefix)
+      latest_folder.children.filter {|e|
+        filename = e.split.last.to_s
+
+        filename.start_with?("#{prefix}__")
+      }
+        .max
+    end
+
+    def latest_folder
+      serialized_data_repository_path.children.max
+    end
+
+    def serialized_data_repository_path
+      Rails.root.join(STORAGE_LOCATION)
+    end
+  end
+end

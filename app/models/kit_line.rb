1036273a103b28a2b0d91347b0033d9d5624d80e
Sean DMR <sean@flyinggrizzly.net>
2022-09-18 18:49:23 +0100

    Add Rails validation of KitLine nickname presence

--- app/models/kit_line.rb
+++ app/models/kit_line.rb
@@ -2,6 +2,7 @@ class KitLine < ApplicationRecord
   belongs_to :producer
 
   validates :name, presence: true, uniqueness: true
+  validates :nickname, presence: true, uniqueness: true
   validates :slug, presence: true, uniqueness: true
 
   before_validation :set_name_as_nickname_if_blank

4592941d55d4df9b661fe0a6d0477ef687094c1f
Sean DMR <sean@flyinggrizzly.net>
2022-09-18 18:46:50 +0100

    Ensure all KitLines have nicknames

--- app/models/kit_line.rb
+++ app/models/kit_line.rb
@@ -4,7 +4,17 @@ class KitLine < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :slug, presence: true, uniqueness: true
 
+  before_validation :set_name_as_nickname_if_blank
+
   def nickname
     super || name
+  end
+
+  private
+
+  def set_name_as_nickname_if_blank
+    return if nickname.present?
+
+    self.nickname = name
   end
 end

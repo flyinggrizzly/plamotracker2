a53a66b55f90a6288cd18072f23b2aed8f6fddb3
Sean DMR <sean@flyinggrizzly.net>
2022-09-18 16:13:25 +0100

    Allow non-numeric scales

--- app/models/kit_scale.rb
+++ app/models/kit_scale.rb
@@ -2,7 +2,6 @@ class KitScale < ApplicationRecord
   has_many :kits
 
   validates :name, presence: true, uniqueness: true
-  validates_numericality_of :denominator
 
   scope :increasing, -> { order(denominator: 'asc') }
   scope :decreasing, -> { order(denominator: 'desc') }

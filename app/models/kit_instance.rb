d7a6d06c50160a2ea7fb4f7c828ee93cf4254db2
Sean DMR <sean@flyinggrizzly.net>
2022-09-19 16:20:45 +0100

    Add KitInstance#guid behavior

--- app/models/kit_instance.rb
+++ app/models/kit_instance.rb
@@ -1,6 +1,8 @@
 class KitInstance < ApplicationRecord
   belongs_to :kit
 
+  delegate :name, to: :kit, prefix: true
+
   STATUSES = {
     NEEDED: 0,
     DESIRED: 1,
@@ -12,10 +14,22 @@ class KitInstance < ApplicationRecord
   }
   enum :status, STATUSES
 
+  before_validation :set_guid, unless: :persisted?
+
+  validates :guid, presence: true, uniqueness: { scope: :kit }
+
   default_scope -> { order(created_at: 'asc') }
   scope :persisted, -> { where.not(created_at: nil) }
 
   def status_name
     status&.humanize
+  end
+
+  private
+
+  def set_guid
+    return if persisted?
+
+    self.guid = "#{kit_name.parameterize}--instance__#{SecureRandom.uuid}"
   end
 end
